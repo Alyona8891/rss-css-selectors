b6a1a7f71bc757f7e4778c1294f33008370a66b9
unknown <besssta888@mail.ru>
2023-07-03 11:49:34 +0300

    fix: fix way to highlighting style

--- rss-css-selectors/src/components/view/main/block3View/htmlViewerView/htmlViewerView.ts
+++ rss-css-selectors/src/components/view/main/block3View/htmlViewerView/htmlViewerView.ts
@@ -1,7 +1,7 @@
 import './htmlViewer.css';
 import { ParamsBlockHtmlElem, ParamsElementCreator } from '../../../../../types/types';
 import View from '../../../view';
-import './kimbie-dark.min.css';
+import 'highlight.js/styles/kimbie-dark.css';
 import hljs from 'highlight.js/lib/core';
 import xml from 'highlight.js/lib/languages/xml';
 import createBlock from '../../../../functions/createBlock';
