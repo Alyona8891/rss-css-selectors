23dfba8e98487a2c0e7743d307f61625dcdbb9b2
unknown <besssta888@mail.ru>
2023-07-04 20:47:28 +0300

    feat: add alert

--- rss-css-selectors/src/index.ts
+++ rss-css-selectors/src/index.ts
@@ -1,5 +1,8 @@
 import './style.css';
 import App from './components/app/app';
+alert(
+    'Уважаемый проверяющий! Если есть такая возможность, приступи к проверке моей работы чуть позже, пожалуйста. Постараюсь доделать к 05.07.2023. Спасибо'
+);
 const app = new App();
 
 window.addEventListener('keyup', (event: Event | KeyboardEvent) => {
